667b0cbdc008fee598fcf6c3d151d549ef3a49ad
northka <northka.chen@gmail.com>
2017-01-02 15:03:55 +0800

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by chenchaochao on 17/1/2.
+ */
+const func      = require('./lib/function')
+const generator = require('./lib/generator')
+
+function interface (fn){
+    if(!fn){
+        throw new Error('interface need function')
+    }
+    if(fn.constructor.name == 'Function'){
+        return func(...arguments)
+    }
+    if(fn.constructor.name == 'GeneratorFunction'){
+        return generator(...arguments)
+    }
+}
